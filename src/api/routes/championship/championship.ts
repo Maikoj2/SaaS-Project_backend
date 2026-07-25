4d5de058f6fb0fdc8005294302fff5bc6d6093c4
Maicol Jimenez <maicol_jimenez82121@elpoli.edu.co>
2026-07-25 18:46:52 -0500

    feat: add Cloudinary image upload for championships

--- src/api/routes/championship/championship.ts
+++ src/api/routes/championship/championship.ts
@@ -3,11 +3,12 @@ import { origin } from '../../middlewares';
 import { auth } from '../../middlewares/auth.middleware';
 import { ChampionshipController } from '../../controllers/championship/championship.controller';
 import trimRequest from 'trim-request';
-import { validateCreateChampionship, validateCreateChampionshipConfiguration, validateRegisterTeam, validateUpdateChampionshipStatus } from '../../validators/championships/championship.validator';
+import { validateCreateChampionship, validateCreateChampionshipConfiguration, validateRegisterTeam, validateSoftDeleteChampionship, validateUpdateChampionshipStatus } from '../../validators/championships/championship.validator';
 import { ChampionshipsRoutes } from '../../constants/apiRoutes/championship/championshipsRoutes';
 import { permissionAuthorization } from '../../middlewares/auth/permissionAuthorization.middleware';
 import { AuthPermission } from '../../constants/permissions';
 import { championshipConfigurationValidators } from '../../validators/championships/Configuration.validator';
+import { uploadLogoImage } from '../../middlewares/uploadImage.middleware';
 
 
 const championshipController = new ChampionshipController();
@@ -25,7 +26,7 @@ router.post(ChampionshipsRoutes.CHAMPIONSHIPS, [
     trimRequest.all,
 ], championshipController.create as RequestHandler);
 
-// get all championships
+// get active championships
 router.get(ChampionshipsRoutes.CHAMPIONSHIPS, [
     origin.checkDomain as RequestHandler,
     origin.checkTenant as RequestHandler,
@@ -80,5 +81,52 @@ router.get(ChampionshipsRoutes.CHAMPIONSHIPS_ID_CONFIGURATION, [
     trimRequest.all,
     ...championshipConfigurationValidators.getChampionshipConfiguration,
 ], championshipController.getChampionshipConfiguration as RequestHandler);
+
+
+router.delete(ChampionshipsRoutes.CHAMPIONSHIPS_DELETE, [
+    origin.checkDomain as RequestHandler,
+    origin.checkTenant as RequestHandler,
+    auth as RequestHandler,
+    permissionAuthorization([AuthPermission.CHAMPIONSHIP_DELETE]) as RequestHandler,
+    trimRequest.all as RequestHandler,
+    ...validateSoftDeleteChampionship,
+], championshipController.softDelete as RequestHandler);
+
+router.get(ChampionshipsRoutes.CHAMPIONSHIPS, [
+    origin.checkDomain as RequestHandler,
+    origin.checkTenant as RequestHandler,
+    auth as RequestHandler,
+    permissionAuthorization([AuthPermission.CHAMPIONSHIP_READ]) as RequestHandler,
+], championshipController.getAll as RequestHandler);
+
+router.patch(
+    ChampionshipsRoutes.CHAMPIONSHIPS_UPLOAD_BANNER,
+    [
+        origin.checkDomain as RequestHandler,
+        origin.checkTenant as RequestHandler,
+        auth as RequestHandler,
+        permissionAuthorization([AuthPermission.CHAMPIONSHIP_UPDATE]) as RequestHandler,
+        uploadLogoImage.single("image"),
+        ...championshipConfigurationValidators.uploadLogoAndBanner,
+        trimRequest.all,
+    ],
+    championshipController.uploadBanner as RequestHandler
+);
+
+router.patch(
+    ChampionshipsRoutes.CHAMPIONSHIPS_UPLOAD_LOGO,
+    [
+        origin.checkDomain as RequestHandler,
+        origin.checkTenant as RequestHandler,
+        auth as RequestHandler,
+        permissionAuthorization([AuthPermission.CHAMPIONSHIP_UPDATE]) as RequestHandler,
+        uploadLogoImage.single("image"),
+        ...championshipConfigurationValidators.uploadLogoAndBanner,
+        trimRequest.all,
+    ],
+    championshipController.uploadLogo.bind(championshipController) as RequestHandler
+);
+
+
 
 export default router;
